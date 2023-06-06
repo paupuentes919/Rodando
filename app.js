a877ce58184d7e33425e6d43947052cb82e73e5b
Virginia Frias <virginiafriasunc2000@gmail.com>
2023-06-06 15:49:12 +0200

    Adelanto de pagina Home - renombre del index a home

--- app.js
+++ app.js
@@ -4,16 +4,22 @@ const path = require("path");
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "/public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/nmb", express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css")));
+app.use(
+    "/nmb",
+    express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css"))
+);
 
-app.use("/scripts", express.static(path.join(__dirname, "node_modules/bootstrap/dist/js")));
+app.use(
+    "/scripts",
+    express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
+);
 
 app.get("/", function (req, res) {
-	res.sendFile(path.join(__dirname, "/views/home.html"));
+    res.sendFile(path.join(__dirname, "/views/home.html"));
 });
 
 app.listen(3010, function (req, res) {
-	console.log("Servidor en puerto 3010 corriendo");
+    console.log("Servidor en puerto 3010 corriendo");
 });
