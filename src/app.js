48e12f58cc54e901cb55458d96a32f973aa3d28b
tomasgomien <tomygomien@gmail.com>
2023-06-23 15:35:56 -0300

    archivos html a ejs, css de carrito

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const path = require('path');
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set('view engine','ejs');
 app.set('views', path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "../public")));
 
