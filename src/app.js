03f222d1f4da897cf2954d13db32454fc93196e4
Virginia Frias <virginiafriasunc2000@gmail.com>
2023-06-25 04:29:29 +0200

    creacion de formularios

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
-const mainRoutes = require('./routes/mainRoutes');
-const productRoutes = require('./routes/productRoutes');
-const userRoutes = require('./routes/userRoutes');
+const mainRoutes = require("./routes/mainRoutes");
+const productRoutes = require("./routes/productRoutes");
+const userRoutes = require("./routes/userRoutes");
 
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
-app.set('view engine','ejs');
-app.set('views', path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 app.use(express.static(path.join(__dirname, "../public")));
 
 app.use(
@@ -21,11 +21,11 @@ app.use(
     express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
 );
 
-app.use('/',mainRoutes);
+app.use("/", mainRoutes);
 
-app.use('/productos',productRoutes);
+app.use("/productos", productRoutes);
 
-app.use('/usuarios',userRoutes);
+app.use("/usuarios", userRoutes);
 
 app.listen(3010, function (req, res) {
     console.log("Servidor en puerto 3010 corriendo");
--- src/routes/productRoutes.js
+++ src/routes/productRoutes.js
@@ -1,11 +1,15 @@
-const productControllers = require('./../controllers/productControllers')
+const productControllers = require("./../controllers/productControllers");
 
-const express = require('express');
+const express = require("express");
 
 const router = express.Router();
 
-router.get("/carrito",productControllers.carrito);
+router.get("/carrito", productControllers.carrito);
 
-router.get("/detalle",productControllers.detalle);
+router.get("/detalle", productControllers.detalle);
+
+router.get("/crear", productControllers.crear);
+
+router.get("/editar", productControllers.editar);
 
 module.exports = router;
