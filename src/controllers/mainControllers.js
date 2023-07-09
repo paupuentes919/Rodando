b17f27b872edec3d4dda59d2ec63a7b7e203647d
Virginia Frias <virginiafriasunc2000@gmail.com>
2023-07-09 17:59:15 +0200

    Organizaion del archivo CSS del home - archivo prittier

--- src/controllers/mainControllers.js
+++ src/controllers/mainControllers.js
@@ -1,10 +1,9 @@
-const path = require('path');
+const path = require("path");
 
 const controlador = {
-
-    home: function(req,res){
-        res.render("home");
-    }
-}
+  home: function (req, res) {
+    res.render("home");
+  },
+};
 
 module.exports = controlador;
