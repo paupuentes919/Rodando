c0d8af4a0a59284f56e833d8bb1915b7bac1f2c8
tomasgomien <tomygomien@gmail.com>
2023-09-08 20:18:14 -0300

    .env subido con elcodigo

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,10 +1,12 @@
+require('dotenv').config();
+
 module.exports = {
   development: {
-    username: "rodandodh",
-    password: "Rodando2023",
-    database: "rodandodh_2023",
-    host: "mysql-rodandodh.alwaysdata.net",
-    dialect: "mysql",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
   },
   test: {
     username: "root",
