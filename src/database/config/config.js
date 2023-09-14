af0d08003e96ba516bed584e0d620d6f38009a60
Virginia Frias <virginiafriasunc2000@gmail.com>
2023-09-14 05:04:39 +0200

    configuracion de variables de entorno actualizado y gitignore modificado

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
   development: {
@@ -9,17 +9,17 @@ module.exports = {
     dialect: process.env.DB_DIALECT,
   },
   test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql",
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
   },
 };
