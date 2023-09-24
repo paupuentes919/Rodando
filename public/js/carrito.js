9045135173fb450778a5d79d4554fbbb59718848
Virginia Frias <virginiafriasunc2000@gmail.com>
2023-09-25 00:37:17 +0200

    avances del carrito

--- public/js/carrito.js
+++ public/js/carrito.js
@@ -1,38 +1,58 @@
-window.addEventListener("load", function () {
-  //DETALLE
-  // Captura de los inputs y botones
-  let tituloRodado = document.querySelector("#titulo-rodado").innerText;
-  let precioUnitario = document.querySelector("#precio-unitario");
-  let btnCarrito = document.querySelector("#btn-carrito");
-  let cantidadRodados = document.querySelector("#cantidad-rodados");
-  let cantidadHoras = document.querySelector("#cantidad-horas");
-  let datepicker = document.querySelector("#datepicker");
+// obtener en carrito (js 2)
+let botonBorrarCarrito = document.getElementById("borrar-Carrito");
+let iconoCarrito = document.getElementById("iconoCarrito");
+// let botonBorrarItem = document.getElementById("borrar-Item");
+let carritoArray = JSON.parse(sessionStorage.getItem("carrito"));
+let precioTotalCarrito = document.getElementById("precio-total");
+let sumatoriaTotal = 0;
 
-  let tituloRodadoCarrito = document.querySelector("#titulo-rodado-carrito");
+carritoArray.forEach((obj) => {
+  let div = document.createElement("div");
+  let section = document.getElementById("div1");
+  sumatoriaTotal = sumatoriaTotal + obj.precioTotal;
+  div.innerHTML = `<div class="div--precio-numero">
+  <div class="display-flex">
+  <div class="div-img-products">
+  <img
+  class="img-width-height"
+  src="https://rodando.alwaysdata.net/bicicleta-1.png"
+  />
+  </div>
+  <div class="div-container-products">
+  <div class="p-titulo-tipo-rodado">
+  <h2>${obj.tituloRodado}</h2>
+  </div>
+  <div class="div-horas-cantidad" >
+  <p class="p-horas-cantidad horas">Horas contratadas: </p>
+  <p>${obj.cantidadHoras}hs</p>
+  </div>
+  <div class="div-vehiculo-cantidad">
+  <p class="p-horas-cantidad cantidad">Vehículos contratados: </p>
+  <p> ${obj.cantidadRodado}</p>
+  </div>
+  </div>
+  </div>
+  <div class="producto-datos-inputs-seleccion">
+  <div class="precio">
+    <label>$${obj.precioTotal}</label>
+    </div>
+    <div class="botones">
+    <button><i class="fa-solid fa-trash-can"></i></button>
+    <button>
+    <a href="paginaDeReserva">
+    <i class="fa-solid fa-pen-to-square"></i>
+    </a>
+    </button>
+    </div>
+    </div>
+    </div>`;
+  section.appendChild(div);
+});
 
-  let array = [];
+precioTotalCarrito.innerHTML = sumatoriaTotal;
 
- // Guardar en SessionStorage cuando se hace click en el btn del carrito
-  btnCarrito.addEventListener("click", function () {
-    for(let i=0; i<tituloRodado.length;i++){
-        array.push(sessionStorage.setItem("tituloRodado", JSON.stringify(tituloRodado)));
-    }
-    
-    sessionStorage.setItem("precioUnitario", precioUnitario.innerText);
-    sessionStorage.setItem("cantidadRodados", cantidadRodados.value);
-    sessionStorage.setItem("cantidadHoras", cantidadHoras.value);
-    sessionStorage.setItem("datepicker", datepicker.value);
-    
-    // tituloRodadoCarrito = JSON.parse(sessionStorage.getItem("tituloRodado"));
-  });
+// Borrado de carrito completo
 
-//   console.log("ver",tituloRodado);
-  
- //CARRITO
- // Captura de los inputs y botones
- 
- 
-//  tituloRodadoCarrito.innerText = tituloRodado.innerText;
- 
- 
+botonBorrarCarrito.addEventListener("click", function () {
+  sessionStorage.clear();
 });
